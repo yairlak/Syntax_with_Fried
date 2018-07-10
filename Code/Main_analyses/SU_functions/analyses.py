ba9724b36dcd57ad4ea842ca211afa6ebdcc0a92
FAIRNS <yair.lakretz@cea.fr>
2018-07-10 12:07:26 +0200

    cleaning code main plot

--- Code/Main_analyses/SU_functions/analyses.py
+++ Code/Main_analyses/SU_functions/analyses.py
@@ -6,6 +6,149 @@ import matplotlib.pyplot as plt
 import pickle
 import matplotlib.image as mpimg
 from operator import itemgetter
+import load_data, convert_to_mne, analyses
+
+def single_unit_analyses(events_spikes, event_id, metadata, comparisons, settings, params, preferences):
+    print('Loading spike sorted data (spike clusters)...')
+    spikes, settings, electrode_names_from_raw_files, from_channels = load_data.spike_clusters(settings)
+
+    print('Generating MNE raw object for spikes...')
+    raw_spikes = convert_to_mne.generate_mne_raw_object_for_spikes(spikes, electrode_names_from_raw_files, settings,
+                                                                   params)
+
+    print('Epoching spiking data...')
+    epochs_spikes = mne.Epochs(raw_spikes, events_spikes, event_id, params.tmin, params.tmax, metadata=metadata,
+                               baseline=None, preload=True)
+    print(epochs_spikes)
+
+    print('Generate rasters and PSTHs...')
+
+    if preferences.use_metadata_only:
+        for contrast_name, curr_query, curr_blocks, curr_align_to, curr_sorting, cond_label in zip(
+                comparisons['contrast_names'], comparisons['queries'], comparisons['blocks'],
+                comparisons['align_to'], comparisons['sortings'], comparisons['cond_labels']):
+            print('Contrast: ' + contrast_name)
+            preferences.sort_according_to_key = [s.strip().encode('ascii') for s in curr_sorting]
+            str_blocks = ['block == {} or '.format(block) for block in eval(curr_blocks)]
+            str_blocks = '(' + ''.join(str_blocks)[0:-4] + ')'
+            if curr_align_to == 'FIRST':
+                str_align = 'word_position == 1'
+            elif curr_align_to == 'LAST':
+                str_align = 'word_position == sentence_length'
+            elif curr_align_to == 'END':
+                str_align = 'word_position == -1'
+            elif curr_align_to == 'EACH':
+                str_align = 'word_position > 0'
+
+            for query_cond, label_cond in zip(curr_query, cond_label):
+                query = query_cond + ' and ' + str_align + ' and ' + str_blocks
+                print('Query: ' + query)
+                analyses.generate_rasters(epochs_spikes[query], query, electrode_names_from_raw_files, from_channels,
+                                          settings, params, preferences)
+
+
+def micro_electrodes_analyses(channels, comparisons, events, event_id, settings, params, preferences):
+    print("Time-frequency analysis of all channels")
+    for channel in channels:
+        settings.channel = channel
+        print('Loading CSC raw data...')
+        raw_CSC_data_in_mat, settings = load_data.micro_electrodes_raw(settings)
+        print 'Analyzing high-gamma for channel ' + str(channel)
+        # Line filter and resample, or load from file
+        file_name_epochs = 'micro_' + settings.hospital + '_' + settings.patient + '_channel_' + str(
+            channel) + '_line_filtered_resampled-epo'
+
+        print('Generating MNE raw object for continuous data...')
+        raw = convert_to_mne.generate_mne_raw_object(raw_CSC_data_in_mat, settings, params)
+
+        print('Line filtering...')
+        raw.notch_filter(params.line_frequency, filter_length='auto', phase='zero')
+
+        print('Epoching data...')
+        if preferences.use_metadata_only:
+            epochs = mne.Epochs(raw, events, event_id, params.tmin, params.tmax, metadata=metadata, baseline=None,
+                                preload=True)
+        else:
+            epochs = mne.Epochs(raw, events, event_id, params.tmin, params.tmax, baseline=None, preload=True)
+        print(epochs)
+
+        print('Original sampling rate:', epochs.info['sfreq'], 'Hz')
+        epochs.resample(params.downsampling_sfreq, npad='auto')
+        print('New sampling rate:', epochs.info['sfreq'], 'Hz')
+
+        # print('Plot paradigm')
+        # fig_paradigm = mne.viz.plot_events(events_spikes, raw.info['sfreq'], raw.first_samp,
+        #                                    event_id=event_id, show=False)
+        # fname = 'paradigm_events_' + settings.hospital + '_' + settings.patient + '.png'
+        # plt.savefig(os.path.join(settings.path2figures, settings.patient, 'misc', fname))
+        # plt.close(fig_paradigm)
+
+        del raw, raw_CSC_data_in_mat
+
+        print('Time-frequency analyses...')
+        event_ids_epochs = epochs.event_id.keys()
+        for band, fmin, fmax in params.iter_freqs:
+            print('Band: ' + band)
+
+            if preferences.use_metadata_only:
+                for contrast_name, curr_query, curr_blocks, curr_align_to, curr_sorting, cond_label in zip(
+                        comparisons['contrast_names'], comparisons['queries'], comparisons['blocks'],
+                        comparisons['align_to'], comparisons['sortings'], comparisons['cond_labels']):
+                    preferences.sort_according_to_key = [s.strip().encode('ascii') for s in curr_sorting]
+                    str_blocks = ['block == {} or '.format(block) for block in eval(curr_blocks)]
+                    str_blocks = '(' + ''.join(str_blocks)[0:-4] + ')'
+                    if curr_align_to == 'FIRST':
+                        str_align = 'word_position == 1'
+                    elif curr_align_to == 'LAST':
+                        str_align = 'word_position == sentence_length'
+                    elif curr_align_to == 'END':
+                        str_align = 'word_position == -1'
+                    elif curr_align_to == 'EACH':
+                        str_align = 'word_position > 0'
+
+                    for query_cond, label_cond in zip(curr_query, cond_label):
+                        file_name = str(band + '_' + settings.patient + '_channel_' + str(settings.channel) + '_Blocks_'
+                                        + curr_blocks + '_' + label_cond + '_' + curr_align_to + '_' + settings.channel_name)
+                        for key_sort in preferences.sort_according_to_key:
+                            file_name += '_' + key_sort + 'Sorted'
+
+                        IX1 = settings.channel_name.find('_0019')
+                        if IX1 == -1:
+                            IX1 = settings.channel_name.find('.ncs')
+                        probe_name = settings.channel_name[0:IX1 - 1]
+                        if (not os.path.isfile(
+                                os.path.join(settings.path2figures, settings.patient, 'HighGamma', probe_name,
+                                             file_name + '.png'))) or settings.overwrite_existing_output_files:
+
+                            query_baseline = query_cond + ' and word_position == 1 and ' + str_blocks
+                            _, _, baseline = analyses.average_high_gamma(epochs[query_baseline], band,
+                                                                         fmin, fmax, params.freq_step, None,
+                                                                         'trial_wise', params)
+
+                            query = query_cond + ' and ' + str_align + ' and ' + str_blocks
+                            if not curr_align_to == 'EACH':
+                                power, power_ave, _ = analyses.average_high_gamma(epochs[query],
+                                                                                  band,
+                                                                                  fmin, fmax, params.freq_step,
+                                                                                  baseline,
+                                                                                  'trial_wise', params)
+                            else:
+                                power, power_ave, _ = analyses.average_high_gamma(epochs[query],
+                                                                                  band,
+                                                                                  fmin, fmax, params.freq_step,
+                                                                                  [],
+                                                                                  'no_baseline', params)
+
+                            power.tmin = epochs.tmin;
+                            power.tmax = epochs.tmax;
+                            power.metadata = epochs[query].metadata
+                            analyses.plot_and_save_high_gamma(power, power_ave, curr_align_to, eval(curr_blocks),
+                                                              probe_name, file_name,
+                                                              settings, params, preferences)
+                        else:
+                            print('File already exists')
+
+
 
 def generate_rasters(epochs_spikes, query, electrode_names_from_raw_files, from_channels, settings, params, preferences):
     for cluster in np.arange(epochs_spikes.info['nchan']):
